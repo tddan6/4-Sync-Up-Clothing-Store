6dc6442febb85294531e1a216b544289a6ce2a11
Phillip <117806991+tddan6@users.noreply.github.com>
2023-05-26 15:30:22 +1000

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const navObserver = new IntersectionObserver((entries) => {
 navObserver.observe(scrollWatcher)
 
 
+
+
+
+// media menu collapse
+
 let MenuItems = document.getElementById("MenuItems");
 
 MenuItems.style.maxHeight = "0px";
@@ -24,3 +29,19 @@ function menutoggle() {
     }
 
 }
+
+// scroll reveal script
+
+const sr = ScrollReveal ({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset: false
+});
+
+sr.reveal('#hero-header',{delay:50, origin:'top'});
+sr.reveal('#hero-p',{delay:750, origin:'top'});
+sr.reveal('#hero-btn',{delay:1550, origin:'top'});
+sr.reveal('#herosplit1',{delay:400, origin:'right'});
+sr.reveal('#herosplit2',{delay:400, origin:'left'});
+sr.reveal('.col-3',{delay:100, origin:'top'});
